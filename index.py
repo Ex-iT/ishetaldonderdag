6a696c30d01603da981797ba18a7bfbec03054ae
Ex-iT <vic.toothpick@gmail.com>
2021-11-23 21:11:53 +0100

    Use Python naming convention

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from flask import Flask, render_template, redirect
-from flask_minify import minify
+from flask import Flask, render_template
 from pytz import timezone
+from flask_minify import minify
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 minify(app=app, html=True, js=True, cssless=True)
 
-def isThursday():
+def is_thursday():
     amsterdam = timezone('Europe/Amsterdam')
     now = amsterdam.localize(datetime.now())
     return now.isoweekday() == 4
@@ -14,8 +14,8 @@ def isThursday():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def home(path):
-    return render_template('index.html', isThursday=isThursday())
+    return render_template('index.html', is_thursday=is_thursday())
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('index.html', isThursday=isThursday()), 404
+def page_not_found(error):
+    return render_template('index.html', is_thursday=is_thursday()), 404
