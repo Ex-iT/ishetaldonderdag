a9c6bb10355f04f714a57330d66b40a9a4c6e8a5
Ex-iT <vic.toothpick@gmail.com>
2023-09-16 12:01:47 +0200

    Added strict Content-Security-Policy

--- index.py
+++ index.py
@@ -2,19 +2,27 @@ from datetime import datetime
 from flask import Flask, render_template
 from pytz import timezone
 from flask_minify import minify
+import secrets
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 minify(app=app, html=True, js=True, cssless=True)
+
+NONCE = ''
 
 def is_thursday():
     amsterdam = timezone('Europe/Amsterdam')
     now = amsterdam.localize(datetime.now())
     return now.isoweekday() == 4
 
+def set_nonce():
+    global NONCE
+    NONCE = secrets.token_urlsafe()
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def home(path):
-    return render_template('index.html', is_thursday=is_thursday())
+    set_nonce()
+    return render_template('index.html', is_thursday=is_thursday(), nonce=NONCE)
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -27,5 +35,6 @@ def after_request(response):
     response.headers['X-XSS-Protection'] = '1; mode=block'
     response.headers['Referrer-Policy'] = 'no-referrer-when-downgrade'
     response.headers['Permissions-Policy'] = 'geolocation=(self), microphone=()'
+    response.headers['Content-Security-Policy'] = f'default-src \'self\'; base-uri \'none\'; object-src \'none\'; style-src \'self\' \'unsafe-inline\' fonts.googleapis.com; font-src \'self\' fonts.gstatic.com; script-src \'self\' \'nonce-{NONCE}\' \'unsafe-inline\' \'strict-dynamic\' https:'
 
     return response
